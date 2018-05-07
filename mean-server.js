dbe820f48355f2ca202edc9abfe95fa0da7a53ff
unknown <jlbarbut@ncsu.edu>
2018-05-07 19:23:37 -0400

    this time with more functions

--- mean-server.js
+++ mean-server.js
@@ -1,7 +1,13 @@
 var http = require("http");
 
-http.createServer(function(request, response){
-    response.end("Maybe you should go to the gym. Or do something worthwile today...");
-}).listen(7500, function(){
+var PORT = 7500;
+
+function handleRequest(request, response){
+    response.end("Maybe you should go to the gym. Or do something worthwile today... ");
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
     console.log("Something mean worked");
 });
